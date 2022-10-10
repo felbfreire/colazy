fd3dcac9a64af317ea74bc02d54a09c6db444e1c
felbfreire <felbfreire@proton.me>
2022-10-10 09:09:00 -0300

    now querry_db is a method of PgColazy

--- src/colazy/wrappers.py
+++ src/colazy/wrappers.py
@@ -3,37 +3,74 @@ from .utils import get_cursor, drop_connection
 import psycopg2
 
 
-def querry_db(fn):
-    """ 
-        wrapps a querry:
+class PgColazy:
 
-        -> connects to db and create a cursor,
-        -> executes a querry (function to be decorated),
-        -> then commits and close connection.
-    """
+	def __init__(self, **kwargs):
+		"""
+		Adapter for connectiong postgres database that
+		holds udeful methods methods.
+		"""
 
-    def wrapper(*args, **kwargs):
+		self.kw = kwargs
 
-        cur = get_cursor()
+		self.dbname = self.kw["dbname"]
+		self.user = self.kw["user"]
+		self.password = self.kw["password"]
 
-        try:
-            to_exec = fn()
-        except TypeError:
-            print("function must return a valid querry")
 
-        try:
-            cur.execute(to_exec)
-        except (psycopg2.Error, TypeError):
-            print("Function must return a valid querry")
+	def get_cursor(self):
+		"""Connects to the database then returns a cursor"""
 
-        try:
-            result = cur.fetchall()
-            cur.connection.close()
-        except psycopg2.Error: 
-            drop_connection(cur)
-            return None
+		conn = psycopg2.connect(
 
-        return result
+			dbname=self.dbname,
+			user=self.user,
+			password=self.password
 
-    return wrapper
+		)
 
+		cur = conn.cursor()
+
+		return cur
+
+
+	def  drop_conn(self, cur):
+		"""commits then drops a cursor connection"""
+
+		cur.connection.commit()
+		cur.connection.close()
+		
+
+	def querry_db(self, fn):
+		""" 
+		wrapps a querry:
+
+		-> connects to db and create a cursor,
+		-> executes a querry (function to be decorated),
+		-> then commits and close connection.
+		"""
+
+		def wrapper(*args, **kwargs):
+
+			cur = self.get_cursor()
+
+			try:
+				to_exec = fn()
+			except TypeError:
+				print("Function must return a valid querry")
+
+			try:
+				cur.execute(to_exec)
+			except (psycopg2.Error, TypeError):
+				print("Function must return a valid querry")
+
+			try:
+				result = cur.fetchall()
+				self.drop_conn(cur)
+			except psycopg2.Error:
+				self.drop_conn(cur)
+				return None
+
+			return result
+
+		return wrapper
